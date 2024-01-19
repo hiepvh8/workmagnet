21a32bd0b5ba8c49c29c755ee469fcf67b8c7c3a
hiepvh8 <hiepvh8@gmail.com>
2024-01-19 18:38:11 +0700

    upload project

---
+++ src/main/java/com/example/workmagnet/WorkmagnetApplication.java
@@ -0,0 +1,13 @@
+package com.example.workmagnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmagnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workmagnet/WorkmagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workmagnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
